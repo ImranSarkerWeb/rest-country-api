3470ed5caea8e964e7d05917e8db2129e4b2b5cc
imransarkerweb <imransarker.web@gmail.com>
2023-03-19 21:50:44 +0600

    dynamicaly added all country to ui

---
+++ js/main.js
@@ -0,0 +1,25 @@
+//https://restcountries.com/v3.1/all
+
+const getCountries = () => {
+  fetch("https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then((data) => displayCountries(data));
+};
+
+const displayCountries = (countries) => {
+  console.log(countries);
+  const allCountriesHtml = countries.map((country) => createUi(country));
+  const countryDiv = document.getElementById("countries");
+  countryDiv.innerHTML = allCountriesHtml.join("");
+};
+
+const createUi = (country) => {
+  return `
+    <div>
+        <h2>${country.name.common}</h2>
+        <img src=${country.flags.png}>
+    <div>
+    `;
+};
+
+getCountries();
