71e58bb9c5fea0484eaa8e99f692651852084c08
imransarkerweb <imransarker.web@gmail.com>
2023-03-19 22:11:04 +0600

    style added

--- js/main.js
+++ js/main.js
@@ -10,15 +10,15 @@ const displayCountries = (countries) => {
   console.log(countries);
   const allCountriesHtml = countries.map((country) => createUi(country));
   const countryDiv = document.getElementById("countries");
-  countryDiv.innerHTML = allCountriesHtml.join("");
+  countryDiv.innerHTML = allCountriesHtml.join(" ");
 };
 
 const createUi = (country) => {
   return `
-    <div>
+    <div class="country">
         <h2>${country.name.common}</h2>
-        <img src=${country.flags.png}>
-    <div>
+        <img src="${country.flags.png}"/>
+    </div>
     `;
 };
 
